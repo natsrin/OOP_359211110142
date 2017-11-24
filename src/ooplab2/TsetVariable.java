647e5f72ba81798608ce4444a61da602b8b52c1b
natsrin <natsarin352@gmail.com>
2017-11-24 10:17:11 +0700

    Initial commit

---
+++ src/ooplab2/TsetVariable.java
@@ -0,0 +1,39 @@
+package ooplab2;
+
+public class TsetVariable {
+        public static void main(String[] args) {
+            // Integer
+            int i;
+            i = 10;
+            System.out.println("i = "+i);
+            int j = 20;
+            System.out.println("j ="+j);
+            System.out.println("i+j="+(i+j));
+            //         Double
+            double v = 15.15;
+            System.out.println(v);
+            System.out.println(v*10);
+            //    Character
+            char c = 'T';
+            System.out.println(c);
+            //      String
+            String s = "Hello,world";
+            System.out.println(s);
+            String name = "I'm BEW";
+            System.out.println(name);
+            String msg = s + " " + name;
+            System.out.println(msg);
+            String msg2 = s.concat(name);
+            System.out.println(msg2);
+
+
+ //
+
+
+
+        }// main
+
+
+
+    }// class
+}
