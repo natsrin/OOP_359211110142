9305ba8e6b6f682ee96adb4a72e2442d1449f84c
LAB_COM-MT_1 <natsarin352@gmail.com>
2017-12-20 17:23:06 +0700

    OverLoadMethod

---
+++ src/oop_lab4/OverLoadMethod.java
@@ -0,0 +1,18 @@
+package oop_lab4;
+//1. Overload method
+//2. Overloading method
+//3. Method Overload
+public class OverLoadMethod {
+    public static void A (){
+        System.out.println("Hello A");
+    }
+     public static void A (int X){
+        System.out.println("Hello A"+X);
+    }
+    public static void A (int x, int y){
+        System.out.println("Hello A"+(x+y));
+    }
+    private static void B (){
+        System.out.println("Hello B");
+    }
+}
